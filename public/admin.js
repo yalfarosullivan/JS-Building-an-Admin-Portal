b11b70083bcfd769b320707d6da177575b905c4d
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-10-21 22:17:27 -0700

    added to admin

--- public/admin.js
+++ public/admin.js
@@ -6,8 +6,9 @@ async function saveShinobi() {
     console.log(inputValue)
     let response = await fetch('http://localhost:3001/listBooks', {
     method: "PATCH"
-    headers:{
+    headers: {
         'Content-Type' : 'application/json'
+        },
         body: JSON.stringify({
             "id" : 1,
             "quantity": inputValue
@@ -23,9 +24,42 @@ async function saveShinobi() {
         method: "PATCH"
         headers:{
             'Content-Type' : 'application/json'
+        },
             body: JSON.stringify({
-                "id" : 1,
+                "id" : 3,
                 "quantity": inputValue
             })
         })
         }
+
+        async function saveAnnals() {
+            let inputValue = document.getElementById("annals-input").value
+            console.log('BUTTON')
+            console.log(inputValue)
+            let response = await fetch('http://localhost:3001/listBooks', {
+            method: "PATCH"
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+                body: JSON.stringify({
+                    "id" : 2,
+                    "quantity": inputValue
+                })
+            })
+            }
+
+            async function saveWarp() {
+                let inputValue = document.getElementById("warp-input").value
+                console.log('BUTTON')
+                console.log(inputValue)
+                let response = await fetch('http://localhost:3001/listBooks', {
+                method: "PATCH"
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                    body: JSON.stringify({
+                        "id" : 1,
+                        "quantity": inputValue
+                    })
+                })
+                }
